2ba4587c158e93cab19bc83934320434796fb12e
Shen Yiming <soimy@163.com>
2018-02-17 18:11:10 +0800

    Update test

--- test/Timer.spec.ts
+++ test/Timer.spec.ts
@@ -1,20 +1,34 @@
 import Timer from "../lib/Timer";
 import { expect } from "chai";
+import TimerManager from "../lib/TimerManager";
 
 describe("Timer", () => {
     let timer: Timer;
+    const manager: TimerManager = Timer.timerManager;
     // beforeEach(() => {
     //     timer = new Timer();
     // });
 
-    context("#Timer intialized", () => {
+    context("#Timer add & delete", () => {
         timer = new Timer(1000);
+        let timer2: Timer;
         it("TimerManager is created", () => {
-            expect(Timer.timerManager).to.not.equal(null);
+            expect(manager).to.not.equal(null);
         });
 
         it("Timer count is 1", () => {
-            expect(Timer.timerManager.timers.length).to.equal(1);
+            expect(manager.timers.length).to.equal(1);
+        });
+
+        it("Add more timers", () => {
+            timer2 = new Timer(2000);
+            expect(manager.timers.length).to.equal(2);
+        });
+
+        it("Delete one timer", () => {
+            manager.removeTimer(timer2);
+            manager.update(500);
+            expect(manager.timers.length).to.equal(1);
         });
     });
 });
