3782eda518f2651e039ac19c78d5ff4dcfb73130
Shen Yiming <soimy@163.com>
2018-02-27 16:20:02 +0800

    Update mocha-webpack config to reflect sourcemap line number

--- webpack.test.js
+++ webpack.test.js
@@ -1,6 +1,7 @@
 const nodeExternals = require('webpack-node-externals');
 const isCoverage = process.env.NODE_ENV === 'coverage';
 const path = require('path');
+const { CheckerPlugin } = require('awesome-typescript-loader')
 
 module.exports = {
     resolve: {
@@ -17,20 +18,17 @@ module.exports = {
             isCoverage ? {
                 test: /\.(js|ts)/,
                 include: path.resolve('src'), // instrument only testing sources with Istanbul, after ts-loader runs
-                enforce: 'post',
                 loader: 'istanbul-instrumenter-loader'
             }: [],
             {
                 test: /\.ts$/,
                 exclude: /(node_modules|bower_components)/,
-                loader: 'awesome-typescript-loader',
-                options: {
-                    declaration: false
-                }
+                loader: 'awesome-typescript-loader?sourceMap=true&declaration=false',
             }
         )
     },
+    plugins: [ new CheckerPlugin() ],
     target: 'node',  // webpack should compile node compatible code
     externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
-    devtool: "inline-cheap-module-source-map"
+    devtool: "#inline-cheap-module-source-map"
 };
